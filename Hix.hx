9a4897bb1941e0f1cde70fb8c03537599e9b6eeb
bncastle <bncastle@gmail.com>
2016-08-11 15:51:01 -0400

    Hix now supports labels for multiple build conifgs
    Hix now supports labels for multiple build conifgs
    Updated README with more instructions.

--- Hix.hx
+++ Hix.hx
@@ -10,38 +10,34 @@
 //Note: The above requires you have the haxe std library dlls installed on the system
 //      where you want to use Hix. That should not be a problem.
 //
-//Or run it as a script: haxe -main Hix --interp Hix.hx
-
 import sys.io.File;
 import sys.FileSystem;
-import haxe.io.Path;
 
 enum State
 {
 	SearchingForHeader;
-	GettingArgs;
+	SearchingForArgs;
 	FinishSuccess;
 	FinishFail;
 }
 
-
 class Hix {
 	static inline var VERSION = "0.3";
-
-	//Name of the executable to run
-	//Note this can be changed by the 'exe' command
-	var exe:String = "haxe";
-
-	//This is the header string that must be present in the file to allow us to parse the
-	//compiler args
+	//The header string that must be present in the file so we know to parse the compiler args
 	static inline var COMMAND_PREFIX = "::";
 	static inline var HEADER_START = COMMAND_PREFIX + "hix";
 	static inline var SPECIAL_CHAR = "$";
+	static inline var DEFAULT_BUILD_NAME = "_";
+	
+	//Name of the executable to run (this can be changed by placing '//::exe=newExe.exe' before the start header)
+	var exe:String = "haxe";
 
-	static function main():Int 	{
-		if(Sys.args().length == 0 ) {
+	static function main():Int 	
+	{
+		if(Sys.args().length == 0 )
+		{
 			Sys.println('== Hix Version $VERSION by Pixelbyte Studios ==');
-			Sys.println('Hix.exe <inputFile.hx> OR');			
+			Sys.println('Hix.exe <inputFile.hx> [buildName] OR');			
 			Sys.println('Hix.exe -h for help');			
 			return 1;
 		}
@@ -54,45 +50,53 @@ class Hix {
 
 		//Get the input file name
 		var inputFile:String = Sys.args()[0];
+		var inputBuildName:String = DEFAULT_BUILD_NAME;
+
+		if(Sys.args().length > 1)
+			inputBuildName = Sys.args()[1];
 
 		//Check if file exists
-		if(!FileSystem.exists(inputFile))	{
+		if(!FileSystem.exists(inputFile))
+		{
 			Sys.println('[Hix] File: $inputFile does not exist!');
 			return 1;
 		}
 
 		var h = new Hix();
-		var args = h.ParseFile(inputFile);
+		h.ParseFile(inputFile);
 
-		if(!h.Execute()) return 1;
-		else return 0;
+		if(h.state == FinishSuccess)
+			return h.Execute(inputBuildName);
+		else
+			return 1;
 	}
-
-	//The Path of this exe
-	var exePath:String;
-
-	var args:Array<String>;
 
 	//Read-only property for the current state of the parser
 	public var state(null,default):State = SearchingForHeader;
 
 	//true if we are currently in a comment, false otherwise
-	var isAComment:Bool = false;
+	var inComment:Bool = false;
+	
+	//Tells us if we are in a multi-line comment
+	var multilineComment:Bool = false;
+
+	//The name of the file under examination
+	var filename:String;
 
 	//The current line of text from the input file
 	var text:String;
 
-	public function new() {
-		args = new Array<String>();
+	var currentBuildName:String = DEFAULT_BUILD_NAME;
 
-		//Get the pateh where this executable resides
-		exePath = Sys.programPath();
-		if(exePath.charAt(exePath.length - 1) == '/')
-			exePath = exePath.substr(0,exePath.length - 1);
-		exePath = Path.addTrailingSlash(exePath);
+	var buildMap: Map<String,Array<String>>;
+
+	public function new() 
+	{
+		buildMap = new Map<String,Array<String>>();
 	}
 
-	function Clear(arr:Array<Dynamic>){
+	function Clear(arr:Array<Dynamic>)
+	{
 		#if (cpp || php)
 			arr.splice(0, arr.length);
 		#else
@@ -103,70 +107,102 @@ class Hix {
 	//
 	// This executes the command that was constructed by Calling ParseFile
 	//
-	public function Execute(): Bool
+	public function Execute(buildName: String): Int
 	{
-		if(state == State.SearchingForHeader) {
-			Sys.println('[Hix] Unable to find the start header in a comment block -> $HEADER_START');
-			return false;
+		if(!buildMap.exists(buildName) || buildMap[buildName].length == 0)
+		{
+			if(buildName == DEFAULT_BUILD_NAME)
+				Sys.println("[Hix] No compiler args found for: default");
+			else
+				Sys.println('[Hix] No compiler args found for: $buildName');
+			return -1;
 		}
-		else if(args.length == 0) {
-			Sys.println("[Hix] No compiler args found!");
-			return false;
-		}
-		else {
+		else
+		{
+			var args = buildMap[buildName];
 			Sys.print("[Hix] Running: ");
 			Sys.println(exe + " " + args.join(" "));
 			Sys.println('');
-			return Sys.command(exe, args) == 0;
+			return Sys.command(exe, args);
 		}
 	}
 
 	//
 	// This function parses the input file to get the command line args in order to run the haxe compiler
 	//
-	public function ParseFile(fileName:String): Array<String> {
-
-		//Clear all state info
-		Clear(args);
-		isAComment = false;
-		inMultiLineComment = false;
+	public function ParseFile(fileName:String)
+	{
+		var currentBuildArgs:Array<String> = null;
+		var prevBuildName:String = DEFAULT_BUILD_NAME;
+		inComment = false;
+		multilineComment = false;
+		filename = fileName;
 
 		//Try to open the file in text mode
 		var reader = File.read(fileName,false);
 		try {
-			trace("Searching for a header..");
+			trace("[Hix] Searching for a header..");
 			while(true) {
 				text = reader.readLine();
 
 				//Update the multiline comment state
 				CheckComment();
 
-				switch (state) {
+				switch (state)
+				{
 					case State.SearchingForHeader:
-						if(IsHeader()) {
-							state = State.GettingArgs;
-							trace("[Hix] Getting args..");
+						currentBuildArgs = IsStartHeader();
+						if(currentBuildArgs != null)
+						{
+							prevBuildName = currentBuildName;
+							trace("[Hix] Header Found!");
+							//Once the header is found, we begin searching for build arguments
+							//Note: Once the search for build args has begun, it will stop
+							//once the 1st non-comment line is reached.
+							if(currentBuildName != DEFAULT_BUILD_NAME)
+								trace('[Hix] Getting build args for: $currentBuildName');
+							else
+								trace("[Hix] Getting build args for: default");
+							state = State.SearchingForArgs;
 						}
 						else
-							ParseCommand();
-					case State.GettingArgs:
-						if(!isAComment && args.length == 0)
+							//Look for any Pre-Header commmands
+							ParsePreHeaderCommands();
+					case State.SearchingForArgs:
+						if(!inComment && currentBuildArgs.length == 0)
 						{
 							Sys.println("[Hix] Error: Unable to find any compiler args in the header!");
 							state = State.FinishFail;
 							break;
 						}
-						else if(!isAComment && args.length > 0)
+						else if(!inComment && currentBuildArgs.length > 0)
 						{
 							trace("[Hix] Success");
+
+							CreateBuilder(currentBuildName, currentBuildArgs);
 							state = State.FinishSuccess;
 							break;
 						}
-						else
+						else //We're in a comment
 						{
-							//we are in a comment, lets see if there is text here
-							//if there is we assume it is a command arg
-							GrabArgs(text);
+							var newArgs = IsStartHeader();
+							if(newArgs != null)
+							{
+								trace("[Hix] Success");
+
+								if(currentBuildName != DEFAULT_BUILD_NAME)
+									trace("[Hix] Getting args for: " + currentBuildName);
+								else
+									trace("[Hix] Getting args..");
+							
+								CreateBuilder(prevBuildName, currentBuildArgs);
+								currentBuildArgs = newArgs;
+							}
+							else
+							{
+								//we are in a comment, lets see if there is text here if there is we assume it is a command arg
+								currentBuildArgs = currentBuildArgs.concat(GrabArgs(text));
+							}
 						}
 					case State.FinishSuccess:
 					case State.FinishFail:
@@ -176,105 +212,150 @@ class Hix {
 		catch(ex:haxe.io.Eof) {}
 		reader.close();
 		//Sys.println(text);
+	}
 
+	function ProcessSpecialCommands(buildArgList: Array<String>)
+	{
 		//DO any string search/replace here
 		//A special string starts with '$' and can contain any chars except for whitespace
 		var sp = new EReg("^\\$([^\\s]+)", "i");
-		for (i in 0...args.length) {
-			if(sp.match(args[i]))
+		for (i in 0...buildArgList.length) 
+		{
+			if(sp.match(buildArgList[i]))
 			{
 				//grab the special text. Split it by the '=' sign
 				//so any parameters come after the =
 				var special = sp.matched(1).toLowerCase().split('=');
 
 				//Process special commands here
-				switch (special[0]) {
+				switch (special[0])
+				{
 					case "filename":
-						args[i] = fileName;
+						buildArgList[i] = filename;
 					case "datetime":
 						var date = Date.now();
-
 						//No date parameters? Ok, just do defaul Month/Day/Year
 						if(special.length == 1)
-							args[i] = DateTools.format(date,"%m/%e/%Y_%H:%M:%S");
+							buildArgList[i] = DateTools.format(date,"%m/%e/%Y_%H:%M:%S");
 						else
-							args[i] = DateTools.format(date,special[1]);
+							buildArgList[i] = DateTools.format(date,special[1]);
 				}
 			}
 		}
-		return args;
 	}
 
-	//Grabs the compiler arguments from the given text
-	//
-	function GrabArgs(txt:String) 	{
-		var isBlank = ~/\s*^\/*\s*$/;
+	function CreateBuilder(buildName:String, args:Array<String>)
+	{
+		trace('Create builder for $buildName');
 
-		if(isBlank.match(txt)) return;
-
-		var remhdr = new EReg("^\\s*" + HEADER_START,"i");
-		var remcomm = ~/\s*\/\//;
-		txt = remcomm.replace(text,"");
-		txt = remhdr.replace(txt,"");
-		txt = StringTools.trim(txt);
-
-		var rgs = ParseSepString(txt, " ");
-		for(a in rgs){
-			args.push(a);
+		if(buildMap.exists(buildName))
+		{
+			if(buildName == DEFAULT_BUILD_NAME)
+				Sys.println('Error: Multiple default Build configs found.');
+			else
+				Sys.println('Error: Build config $buildName already exists!');
+			state = State.FinishFail;
+			return;
 		}
+
+		//Now process any special commands
+		ProcessSpecialCommands(args);
+
+		//Add this map to our list of build configs
+		buildMap[buildName] = args;
 	}
 
 	//Looks for the start header string
+	//returns: null if no header found or a new args array
 	//
-	function IsHeader():Bool {
-
+	function IsStartHeader():Array<String>
+	{
 		//If we are not in a comment, return
-		if(!isAComment) return false;
+		if(!inComment) return null;
 
-		var header = new EReg(HEADER_START + "\\s*([^\\n]*)$","i");
+		var header = new EReg(HEADER_START + "(:\\w+)?\\s*([^\\n]*)$","i");
 
 		//See if there is any stuff after the header declaration
-		if(header.match(text)) {
+		if(header.match(text))
+		{
 			var isBlank = ~/^\s*$/;
-			if(!isBlank.match(header.matched(1))) {
+			
+			//Did we find a buildName?
+			if(!isBlank.match(header.matched(1)) && header.matched(1).length > 1)
+				currentBuildName = header.matched(1).substr(1);
+			else 
+				currentBuildName = DEFAULT_BUILD_NAME;
+
+			if(!isBlank.match(header.matched(2)))
+			{
 				//Add this arg to our args arry and trim any whitespace
-				GrabArgs(header.matched(1));
+				return GrabArgs(header.matched(2));
 			}
-			return true;
+			return new Array<String>();
 		}
-		return false;
+		return null;
+	}
+
+
+	//Grabs the compiler arguments from the given text
+	//
+	function GrabArgs(txt:String): Array<String> 
+	{
+		var isBlank = ~/\s*^\/*\s*$/;
+
+		if(isBlank.match(txt)) return null;
+		var args:Array<String> = new Array<String>();
+
+		var remhdr = new EReg("^\\s*" + HEADER_START,"i");
+		var remcomm = ~/\s*\/\//;
+		txt = remcomm.replace(txt,"");
+		txt = remhdr.replace(txt,"");
+		txt = StringTools.trim(txt);
+
+		var parsedArgs = ParseSepString(txt, " ");
+		for(a in parsedArgs)
+		{
+			args.push(a);
+		}
+
+		if(args.length > 0)
+			return args;
+		else 
+			return null;
 	}
 
 	//
-	//Look for any commands that occur before the header start sequence
+	//Look for any commands that occur BEFORE the header start sequence
 	//
-	function ParseCommand()
+	function ParsePreHeaderCommands()
 	{
 		//If we are not in a comment, return
-		if(!isAComment) return;
+		if(!inComment) return;
 
 		var cmd = new EReg(COMMAND_PREFIX + "\\s*([^\\n]*)$","i");
 
-		if(cmd.match(text)) {
+		if(cmd.match(text) && cmd.matched(1).indexOf('=') > -1) {
 			var cmd = cmd.matched(1).toLowerCase().split('=');
 			trace('[Hix] Found command: ${cmd[0]}');
 
-			switch(cmd[0]) {
+			switch(cmd[0]) 
+			{
+				//Ah, we have found the command that says to run a different .exe
+				//so change the exe to what is specified
 				case "exe":
-					if(cmd.length > 1 && cmd[1].length > 0) {
+					if(cmd.length > 1 && cmd[1].length > 0)
+					{
 						exe = cmd[1];
-						trace('Changing exe to: ${cmd[1]}');
+						Sys.println('Hix: exe changed to: ${cmd[1]}');
 					}
 			}
 		}
 	}
 
-	//Tells us if we are in a multi-line comment
-	var inMultiLineComment:Bool = false;
-
 	//Looks for single and multi-line comments
 	//
-	function CheckComment() {
+	function CheckComment() 
+	{
 		//Check for a line comment with nothing but optional spaces before it starts
 		var singleComment = ~/^\s*\/\//;
 		//2 different ways of instantiating an EReg
@@ -283,26 +364,27 @@ class Hix {
 
 		//if we weren't in a comment, see if we are now
 		//Otherwise see if we are out of the comment
-		if(!inMultiLineComment) {
-			inMultiLineComment = begin.match(text) && !end.match(text);
+		if(!multilineComment)
+		{
+			multilineComment = begin.match(text) && !end.match(text);
 
 			//See if we have a line comment
-			isAComment = begin.match(text) || singleComment.match(text);
+			inComment = begin.match(text) || singleComment.match(text);
 		}
-		else {
-			inMultiLineComment = !end.match(text);
+		else
+		{
+			multilineComment = !end.match(text);
 
 			//even if the multiline comment ends, this line is still a comment
-			isAComment = true;
+			inComment = true;
 		}
-
-		//Sys.println('$isAComment: $text');
 	}
 
 	//
 	//This parses a string separated by the given delim and deals with quoted values
 	//
-	static function ParseSepString(text:String, delim:String, removeQuotes:Bool = true) : Array<String> {
+	static function ParseSepString(text:String, delim:String, removeQuotes:Bool = true) : Array<String> 
+	{
 	    var cols:Array<String> = new Array<String>();
 	    var start = 0;
 	    text = StringTools.trim(text);
@@ -313,7 +395,8 @@ class Hix {
 	              var endQuote = text.indexOf('"', start + 1);
 	              if(endQuote == -1)
 	                   throw("[Hix] Parse Error: Expected a matching \"" + text);
-	              else if(text.charAt(endQuote + 1) == '"')  {
+	              else if(text.charAt(endQuote + 1) == '"')
+				  {
 	              	endQuote++;
 	              	endQuote = text.indexOf('"', endQuote + 1);
 
@@ -321,11 +404,13 @@ class Hix {
 						endQuote++;
 	              }
 
-	              if(removeQuotes) {
+	              if(removeQuotes)
+				  {
 	              	cols.push(text.substr(start + 1, endQuote - (start + 1)));
 	              	//trace("[Hix] DQ: " + text.substr(start + 1, endQuote - (start + 1)));
 				  }
-				  else {
+				  else
+				  {
 	              	cols.push(text.substr(start, endQuote + 1 - start));
 	              	//trace("[Hix] DQ: " + text.substr(start , endQuote - start));
 				  }
@@ -337,11 +422,13 @@ class Hix {
 	              if(endQuote == -1)
 	                   throw("[Hix] Parse Error: Expected a matching '\n" + text);
 
-	              if(removeQuotes) {
+	              if(removeQuotes) 
+				  {
 		              cols.push(text.substr(start + 1, endQuote - (start + 1)));
 		              //trace("[Hix] Q: " + text.substr(start + 1, endQuote - (start + 1)));
 	          	  }
-	          	  else {
+	          	  else 
+				  {
 		              cols.push(text.substr(start, endQuote + 1 - start));
 		              //trace("[Hix] Q: " + text.substr(start, endQuote - start ));
 	          	  }
@@ -350,7 +437,8 @@ class Hix {
 	         }
 	         else if(text.charAt(start) == " " )
 	         	start++;
-	         else if(text.charAt(start) == delim)  {
+	         else if(text.charAt(start) == delim)  
+			 {
 	              start++;
 	              //Check and see if it is a null value
 	              while(start < end && text.charAt(start) == " ") start++;
@@ -360,11 +448,13 @@ class Hix {
 	              	//cols.push("");
 	              }
 	         }
-	         else {
+	         else 
+			 {
 	              var lastChar = text.indexOf(delim, start);
 	              if(lastChar == -1)
 	                   lastChar = end;
-	              if(lastChar - start > 0)  {
+	              if(lastChar - start > 0)  
+				  {
 	                   cols.push(text.substr(start, lastChar - start));
 	                   //trace(text.substr(start, lastChar - start));
 	                   start = lastChar;
@@ -392,6 +482,16 @@ $HEADER_START
 -neko hi.n
 */
 
+Hix also supports multiple build configs. Want to be able
+to build either a cpp or neko target? Ok.
+//$HEADER_START::target1 -main Main --no-traces -dce full -cpp bin
+//$HEADER_START:::target2 -main HR --no-traces -dce full -neko hr.n
+
+Then invoke your desired build config by adding the name of the config (which
+in this case is either target1 or target2):
+hix Main.hx target1 <- Builds the cpp target
+hix Main.hx target2 <- Builds the neko target
+
 Then compile by running:
 hix <inputFile.hx>
 
@@ -404,7 +504,7 @@ $datetime -> without specifying a strftime format will output: %m/%e/%Y_%H:%M:%S
 
 You can also change the program that is executed with the args (by default it is haxe)
 by placing a special command BEFORE the start header:
-::exe=<name of executable>
+//::exe=<name of executable>
 
 =============================================================================
 			";
--- README.md
+++ README.md
@@ -6,12 +6,64 @@ Hix removes the need for build.hxml files or command line arguments to the haxe 
 Instead, the compilation flags are stored in the source code where the main class is located
 using special headers stored in comments near the top of the file.
 
+## Building
+
+To build, open a command prompt to the Hix directory and type:
+
+*`haxe.exe build.hxml`*
+
 ## How to Use
 
-TODO: Describe the various options available
+Ex: To build Main.hx as a cpp target, simply add the following in a comment near the top of the Main.hx file:
 
-Any valid argument to Haxe.exe is supported
+```
+//hix:: -main Main --no-traces -dce full -cpp bin
+```
 
-in the end, you type: *hix file.hx*
+The `hix::` text fragment above tells Hix to expect build arguments and to start parsing them. Any build config
+args must begin with this. The above comment residing in Main.hx then enables that build configuration to begin
+executed by typing: *`hix Main.hx`*
 
-to build from Haxe source.
+Hix also understands multi-line build arguments so the following is also valid:
+
+```
+//hix::
+-main Main
+--no-traces
+-dce full
+-cpp bin
+```
+
+Hix also supports multiple different build configs which makes it easy to build for different targets:
+```
+//hix:: -main Main --no-traces -dce full -cpp bin
+//::hix:label1 -main HR --no-traces -dce full -neko hr.n
+//::hix:moon -main HR -lua main.lua
+```
+
+With the above chunk of comments in place, the cpp target is the DEFAULT and can be built as before
+by executing: *`hix Main.hx`* on the command line. Note that the other two lines are a bit different.
+They have labels. Note the 'label1' and 'moon' text in each of the lines. These labels can be any text
+desired and now allow us to change build targets by simply adding the label at the end. Building the
+neko target is simply a matter of typing: *`hix Main.hx label1`* and your build begins! Any valid argument
+the Haxe compiler is supported. To get a list of them, just execute Haxe at the command line with no arguments.
+
+## Special parameters
+
+Hix also supports some special parameters that can be used as arguments to some of the Haxe command lines switches.
+Note that all of these special parameters have the *`$`* prefix.
+
+*`$filename`* inserts the name of the current file.
+
+*`$datetime`* outputs the current date in the following format: %m/%e/%Y_%H:%M:%S
+
+*`$datetime=(formatString)`*  outputs the current date using the given strftime format specification. 
+(Not all strftime settings are supported.)
+
+When Hix is executed, it normally runs `Haxe.exe`, but this can be changed by placing the following
+special command BEFORE any `hix::` headers:
+```
+//::exe=app.exe
+```
+With the above command placed before any `hix::` headers, Hix will no longer execute the given build arguments
+using the default `Haxe.exe`, but will instead use `app.exe`
