fd2f2489c83b2c18e22563f54928114ee62b533c
bncastle <bncastle@pixelbytestudios.com>
2026-04-14 14:59:15 -0400

    Updated haxe template

--- file_templates/hx.hx
+++ file_templates/hx.hx
@@ -1,8 +1,8 @@
 //This program can be compiled with the Hix.exe utility
 ::if (author != null):://Author: ::author::::else:://::end::
 ::if (setupEnv != null):://::SetupKey:: ::setup_env::::else:://::end::
-//::hix       -main ${filenameNoExt} ::if (SrcDir != null)::-cp ::SrcDir::::else::::end:: -D analyzer -cpp bin -D no-traces -dce full
-//::hix:debug -main ${filenameNoExt} ::if (SrcDir != null)::-cp ::SrcDir::::else::::end:: -cpp bin
+//::hix       -main ${filenameNoExt} ::if (SrcDir != null)::-cp ::SrcDir::::else::::end:: -cpp bin -dce full -D analyzer-optimize -D release -D no-traces -D HXCPP_M64
+//::hix:debug -main ${filenameNoExt} ::if (SrcDir != null)::-cp ::SrcDir::::else::::end:: -cpp bin -D HXCPP_M64
 //::hix:run   -main ${filenameNoExt} ::if (SrcDir != null)::-cp ::SrcDir::::else::::end:: --interp
 //
 class ::class_name:: {
